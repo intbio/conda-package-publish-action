51f370a09b2f9b105fd15733f81d55e721326d9f
Alexey Shaytan <molsim@users.noreply.github.com>
2020-01-20 14:33:15 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,8 +17,8 @@ check_if_setup_file_exists() {
 }
 
 check_if_meta_yaml_file_exists() {
-    if [ ! -f meta.yaml ]; then
-        echo "meta.yaml must exist in the directory that is being packaged and published."
+    if [ ! -f conda-recipe/meta.yaml ]; then
+        echo "meta.yaml must exist in the conda-recipe directory that is being packaged and published."
         exit 1
     fi
 }
@@ -26,7 +26,7 @@ check_if_meta_yaml_file_exists() {
 upload_package(){
     conda config --set anaconda_upload yes
     anaconda login --username $INPUT_ANACONDAUSERNAME --password $INPUT_ANACONDAPASSWORD
-    conda build .
+    conda build -c conda-forge conda-recipe
     anaconda logout
 }
 
