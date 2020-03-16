2c0997047f0d71b026a571c1d531ddd847fa2448
Alexey Shaytan <molsim@users.noreply.github.com>
2020-03-16 19:02:19 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,7 +29,7 @@ upload_package(){
     conda config --set anaconda_upload yes
     mkdir output
     anaconda login --username "$ANACONDA_USERNAME" --password "$ANACONDA_PASSWORD" 
-    conda build -c conda-forge conda-recipe --output-folder output
+    conda build -c conda-forge -c bioconda -c intbio conda-recipe --output-folder output
     PKG=`find output -name '*.tar.bz2'`
     conda convert --platform all $PKG -o output/
     find output -name '*.tar.bz2' -exec anaconda upload --force {} \;
